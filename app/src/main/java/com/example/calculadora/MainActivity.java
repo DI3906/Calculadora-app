c8a2418512893ba38d0c77bc261ffbef82d459f2
Diego Garcia <diegg066.99@gmail.com>
2023-09-15 07:53:15 -0600

    Inicio App Calculadora

---
+++ app/src/main/java/com/example/calculadora/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.calculadora;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText num1, num2;
+    TextView resultado;
+    double numero1, numero2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        /**/
+    }
+}
